6538190fdd0e17e6bb5d4c2bfebdd23c8bc25b45
Joshua Kroslowitz <josh@Joshuas-MacBook-Pro.local>
2022-06-17 12:33:38 +0800

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import darkTheme from "./theme/darkTheme";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={''}/>
-      <Route path='news' element={''} />
-      <Route path='blog' element={''} />
-      <Route path='merch' element={''} />
-      <Route path='tour' element={''} />
-    </Routes>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="news" element={""} />
+        <Route path="blog" element={""} />
+        <Route path="merch" element={""} />
+        <Route path="tour" element={""} />
+      </Routes>
+      <Footer />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
